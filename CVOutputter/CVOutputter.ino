ccff9842aa936b590237cff6928019b2b5ee02e7
pkuhanan <pkuhanan@edu.uwaterloo.ca>
2017-07-14 15:43:04 -0400

    Release 1.0

--- CVOutputter/CVOutputter.ino
+++ CVOutputter/CVOutputter.ino
@@ -5,33 +5,22 @@
 #include <SD.h>
 
 
-int pitchPin = A7;
-int volumePin = A6;
-AudioSynthWaveformDc dc0;
-AudioSynthWaveformDc dc1;
-
-//AudioOutputAnalog dac0(0);
-//AudioOutputAnalog dac1(1);
-//
-//AudioConnection patchCord(dc0,dac0);
-//AudioConnection patchCord(dc1,dac1);
+int volumePin = A7;
+int onOffPin = 20;
 
 void setup() {
   AudioMemory(12);
   Serial.begin(9600);
-  pinMode(pitchPin, INPUT);
-  pinMode(volumePin, INPUT);
+
+  pinMode(onOffPin, INPUT);
 }
 
 void loop() {
-  int pitchRead = pulseIn(pitchPin, HIGH);
-  Serial.print("Pitch: ");
-  Serial.println(pitchRead);
+  int volRead = analogRead(volumePin);
+  int isOn = digitalRead(onOffPin);
   
-  
-  int volRead = pulseIn(volumePin, HIGH);
-  Serial.print("Volume: ");
-  Serial.println(volRead);
-//  
-  delay(100);
+  if (isOn == LOW) {
+    volRead = 0;
+  }
+  analogWrite(A22, volRead*255.0/1024.0);
 }
